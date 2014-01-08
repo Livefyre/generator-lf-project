ac1bd319c7698eebfe4d2cad58d322d0d658f920
Chris Richards <crichards@livefyre.com>
2014-01-07 17:32:00 -0800

    doing a copy of the basic settings from bootstrap

---
+++ app/index.js
@@ -0,0 +1,51 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var LfProjectGenerator = module.exports = function LfProjectGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(LfProjectGenerator, yeoman.generators.Base);
+
+LfProjectGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.someOption = props.someOption;
+
+    cb();
+  }.bind(this));
+};
+
+LfProjectGenerator.prototype.app = function app() {
+  this.mkdir('bin');
+  this.mkdir('src');
+  this.mkdir('tests');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+};
+
+LfProjectGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
